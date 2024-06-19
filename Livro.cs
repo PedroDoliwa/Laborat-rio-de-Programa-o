e8e7cf43209f42e1d29ac91aa604f6be6050fcfa
Pedro Henrique <61064109+PedroDoliwa@users.noreply.github.com>
2024-06-18 21:14:02 -0300

    Add files via upload

---
+++ Livro.cs
@@ -0,0 +1,33 @@
+public class Livro{
+
+    public string titulo;
+    public double preco;
+    public Autor autor;
+
+    public Livro (string Rtitulo, Autor Rautor){
+        titulo = Rtitulo;
+        autor = Rautor;
+    }
+
+        public Livro (string Rtitulo, Autor Rautor, double Rpreco){
+        titulo = Rtitulo;
+        autor = Rautor;
+        preco = Rpreco;
+    }
+
+    public void MostrarInfo(){
+         Console.WriteLine($"Titulo: {titulo}\nPreco: {preco}\nAutor: {autor}");
+    }
+
+    public void AplicarDesconto(double Vdesc){
+        preco = preco - (preco*(Vdesc/100));
+        Console.WriteLine($"Preco atual: {preco}");
+    }
+
+    public void AplicarDesconto(int Vdesc){
+        preco = preco - Vdesc;
+            Console.WriteLine($"Preco atual: {preco}");
+
+    }
+
+}
